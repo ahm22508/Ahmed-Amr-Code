65726f44319e7fa9c81c0c7ed68b2209b88f7498
ahm22508 <ahmadamr659@gmail.com>
2024-04-01 22:23:00 +0200

    coomit

--- src/test/java/SwagLabs/LoginTests.java
+++ src/test/java/SwagLabs/LoginTests.java
@@ -2,49 +2,13 @@ package SwagLabs;
 
 import PagesOfSwagLabs.HomePage;
 import PagesOfSwagLabs.LoginPage;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+
 
 public class LoginTests extends BaseTest {
-@DataProvider(name = "LoginTestCase")
-    public Object[][]ExtractFileExcel() throws IOException {
-       File fileExcel = new File(System.getProperty("user.dir")+"\\TestData.xlsx");
-        FileInputStream file = new FileInputStream(fileExcel);
-        XSSFWorkbook workbook = new XSSFWorkbook(file);
-        XSSFSheet sheet = workbook.getSheet("Login");
-        int NumRow = sheet.getLastRowNum();
-        int NumCol= sheet.getRow(0).getLastCellNum();
-        Object[][]ArrayFile = new Object[NumRow][NumCol];
-        for(int r = 0; r < NumRow; r++){
-            XSSFRow row = sheet.getRow(r+1);
-            for (int c=0; c<NumCol; c++) {
-                XSSFCell cell = row.getCell(c);
-                CellType cellType = cell.getCellType();
-                switch (cellType) {
-                    case STRING:
-                        ArrayFile[r][c] = cell.getStringCellValue();
-                        break;
-                    case NUMERIC:
-                        ArrayFile[r][c] = Integer.toString((int) cell.getNumericCellValue());
-                        break;
-                    case BOOLEAN:
-                        ArrayFile[r][c] = cell.getBooleanCellValue();
-                        break;
-                }
-            }
-        }
-return ArrayFile;
-    }
-    @Test(dataProvider = "LoginTestCase")
+@Test(dataProvider = "Login")
     public void validLogin(String UserName, String Password) {
         LoginPage login = new LoginPage(driver);
         HomePage home = new HomePage(driver);
@@ -52,7 +16,23 @@ return ArrayFile;
         boolean AssertLogin = driver.findElement(home.GetHeader()).isDisplayed();
         Assert.assertTrue(AssertLogin);
     }
+
+    @DataProvider(name = "Login")
+    public Object[][] DataProviderMethod(){
+    Object[][] data = new Object[3][2];
+    data[0][0]= "standard_user";
+    data[0][1]= "secret_sauce";
+    data[1][0]= "problem_user";
+    data[1][1]= "secret_sauce";
+    data[2][0]="performance_glitch_user";
+    data[2][1]="secret_sauce";
+
+    return data;
     }
+
+}
+
+
 
 
 
