5344acac21d8e6470c63e2e301f8ecf30723ede1
ahm22508 <ahmadamr659@gmail.com>
2024-04-21 09:18:55 +0200

    commit

--- src/test/java/ParaBankTests/BillPayTest.java
+++ src/test/java/ParaBankTests/BillPayTest.java
@@ -2,25 +2,40 @@ package ParaBankTests;
 
 import ParaBankPages.FinishPage;
 import ParaBankPages.LoginPage;
-import com.github.javafaker.Faker;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.sql.*;
+
+
 public class BillPayTest extends Base {
-        String Name;
-        String Address = "Shoubra";
-        String City = "Cairo";
-        String State = "Shoubra";
-        String ZipCode = "55690";
-        String Phone = "01550810058";
-        String Account = "493449";
-        String VerifyAccount = "493449";
-        String Amount = "500";
-        Faker faker = new Faker();
-    @Test
-    public void PayBills(){
-        System.out.println(faker.name());
-        new LoginPage(driver).Login(RegisterAndLoginTests.UserName, RegisterAndLoginTests.Password).ClickOnBillPay().BillPay(Name,Address, City, State, ZipCode, Phone, Account, VerifyAccount,Amount);
-        Assert.assertTrue(driver.findElement(FinishPage.PillPayComplete()).isDisplayed());
+    @DataProvider(name = "SQL")
+    public String[][] ReadFromDataBaseSQL() throws SQLException{
+        String Query = "Select * from login";
+        Connection Connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/datatesting", "root", "aamrfou55@55");
+        Statement Statement = Connect.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet result = Statement.executeQuery(Query);
+        int Columns = result.getMetaData().getColumnCount();
+        result.last();
+        int Rows = result.getRow();
+        result.beforeFirst();
+        String [][] FinalResult = new String[Rows][Columns];
+        int i = 0;
+        while (result.next()){
+            for (int Z = 0; Z<Columns; Z++){
+                FinalResult[i][Z] = result.getString(Z+1);
+            }
+            i++;
+        }
+    return FinalResult;
+    }
+
+@Test(dataProvider = "SQL")
+public void PayBills(String Name, String Address, String City, String State, String ZipCode, String Phone, String Account, String VerifyAccount, String Amount){
+new LoginPage(driver).
+        Login(RegisterAndLoginTests.UserName, RegisterAndLoginTests.Password).
+        ClickOnBillPay().BillPay(Name,Address, City, State, ZipCode, Phone, Account, VerifyAccount,Amount);
+Assert.assertTrue(driver.findElement(FinishPage.PillPayComplete()).isDisplayed());
     }
 }
